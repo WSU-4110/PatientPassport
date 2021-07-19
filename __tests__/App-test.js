c3a194caf3da3339b44800d7649de6b561ff7962
mhamad9544 <47550256+mhamad9544@users.noreply.github.com>
2021-07-19 19:43:07 -0400

    Update App-test.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,40 @@
-/**
- * @format
- */
-
-import 'react-native';
+var assert = require('assert');
+import {render, fireEvent} from '@testing-library/react-native';
 import React from 'react';
-import App from '../App';
+import Homescreen from '../Screens/Homescreen';
+import ToggleableText from '../Components/ToggleableText';
+import firestore from '@react-native-firebase/firestore';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('Homescreen Test Suite', function () {
+  test('Testing GenderButton', function () {
+    const s = render(<Homescreen></Homescreen>);
+    s.GenderButton({Gender: 'Male'});
+    assert.ok(s.values['firstName'] === 'Male');
+  });
+  test('Testing AllergyButton', function () {
+    const s = render(<Homescreen></Homescreen>);
+    s.AllergyButton({Allergies: 'None'});
+    assert.ok(s.values['ALLER'] === 'None');
+  });
+  test('Testing DisButton', function () {
+    const s = render(<Homescreen></Homescreen>);
+    s.DisButton({Disabilities: 'None'});
+    assert.ok(s.values['dis'] === 'None');
+  });
+  test('Testing VaccineButton', function () {
+    const s = render(<Homescreen></Homescreen>);
+    s.VaccineButton({Vaccinations: 'None'});
+    assert.ok(s.values['Vaccine'] === 'None');
+  });
+  test('Testing MedButton', function () {
+    const s = render(<Homescreen></Homescreen>);
+    s.MedButton({Medications: 'None'});
+    assert.ok(s.values['med'] === 'None');
+  });
+  test('Testing SmokeButton', function () {
+    const s = render(<Homescreen></Homescreen>);
+    s.SmokeButton({Smoke: 'No'});
+    assert.ok(s.values['smk'] === 'No');
+  });
+  
 });
